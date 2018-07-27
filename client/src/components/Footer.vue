baa11404b37d6f94dc4ed7c5eff5bcea2eae5b48
Katy <>
2018-07-28 00:07:15 +0800

    footer description

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -20,7 +20,7 @@
       </v-card-text>
 
       <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        This application is built for all teachers to build their online teaching meterial. For any enquiry, please contact 6353 9312 and find Kiki Chan.
       </v-card-text>
 
       <v-divider></v-divider>
