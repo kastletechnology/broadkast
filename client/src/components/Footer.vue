715453babada7b1c4a78610d4ce478f9cbc80c79
Katy <>
2018-07-27 23:48:37 +0800

    add icons

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -26,7 +26,7 @@
       <v-divider></v-divider>
 
       <v-card-text class="white--text">
-        &copy;2018 — <strong>Vuetify</strong>
+        &copy;2018 — <strong>Kastle Technology</strong>
       </v-card-text>
     </v-card>
   </v-footer>
--- client/src/main.js
+++ client/src/main.js
@@ -7,12 +7,21 @@ import Vuetify from 'vuetify'
 import { sync } from 'vuex-router-sync'
 import 'vuetify/dist/vuetify.min.css'
 import store from '@/store/store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
 
 sync(store, router)
+
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
