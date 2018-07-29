e5040e0933b482ecf61eddf49369071dfed23ee9
Kiki <>
2018-07-29 23:47:51 +0800

    Updated the banner in index page with cute cute dogs

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -14,21 +14,24 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Index',
   data () {
     return {
       items: [
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          src: 'https://d2mga161gr9g8n.cloudfront.net/1200/posts/201612221510-KYEf.jpg'
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          src: 'http://i.imgur.com/luYrZY0.jpg'
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          src: 'https://cdn2.ettoday.net/images/1696/d1696738.jpg'
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          src: 'http://image.zixundingzhi.com/5pGixwaGPab2C7TczOIOSK8_nGo=/660x0/full/0c651260eba5e55986afb2b96a3627e89e1a5352'
+        },
+        {
+          src: 'http://images.china.cn/attachement/jpg/site1000/20151229/001aa0bce27417ec5dc436.jpg'
         }
       ],
       msg: 'Welcome to Your Vue.js App'
