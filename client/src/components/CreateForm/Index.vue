feb3708b1a047fa2b49865e83b31e05f92cd42de
Kiki <>
2018-09-20 00:32:13 +0800

    Added another dynamic form

--- client/src/components/CreateForm/Index.vue
+++ client/src/components/CreateForm/Index.vue
@@ -2,14 +2,15 @@
 <div class="container">
   <v-layout>
     <v-flex>
-      <v-btn dark class="cyan" @click="addNewEmployeeForm"> New Form </v-btn>
-      <panel title="Song Metadata" class="ml-2 mt-2" v-for="(employee, index) in employees" :key="index">
+      <v-btn dark class="cyan" @click="addNewEmployeeForm"> New Employee </v-btn>
+      <v-btn dark class="cyan" @click="addNewWorksheet"> New Worksheet </v-btn>
+      <panel class="ml-2 mt-2" v-for="(employee, index) in employees" :key="index" :title="employee.type">
         <h4 class="card-title" style="float:left"> Add new form (index: {{ index }})</h4>
         <span style="cursor:pointer; float:right;" @click="deleteEmployeeForm(index)">
           X
         </span>
 
-        <div class="mb-3">
+        <div class="mb-3" v-if="employee.name!=null">
           <v-text-field
             label="name"
             required
@@ -31,6 +32,22 @@
             v-model="employee.about"
           ></v-text-field>
         </div>
+
+        <div class="mb-3" v-if="employee.title!=null">
+          <v-text-field
+            label="title"
+            required
+            :rules="[required]"
+            v-model="employee.title"
+          ></v-text-field>
+
+          <v-text-field
+            label="content"
+            required
+            :rules="[required]"
+            v-model="employee.content"
+          ></v-text-field>
+        </div>
       </panel>
     </v-flex>
   </v-layout>
@@ -43,11 +60,7 @@ export default {
     return {
       // Song with null data at the beginning
       employees: [
-        {
-          name: '',
-          job: '',
-          about: ''
-        }
+        // empty at the beginning
       ],
       error: null,
       // Make sure the value is defined
@@ -57,6 +70,7 @@ export default {
   methods: {
     addNewEmployeeForm () {
       this.employees.push({
+        type: 'Employee',
         name: '',
         job: '',
         about: ''
@@ -64,6 +78,13 @@ export default {
     },
     deleteEmployeeForm (index) {
       this.employees.splice(index, 1)
+    },
+    addNewWorksheet () {
+      this.employees.push({
+        type: 'Worksheet',
+        title: '',
+        content: ''
+      })
     }
   }
 }
